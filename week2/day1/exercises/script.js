d593e0791a92819cf2d4d996439b3dd43bed0c99
henna storch <henna.storch@gmail.com>
2022-06-26 12:03:16 +0300

    class

--- week2/day1/exercises/script.js
+++ week2/day1/exercises/script.js
@@ -1,12 +1,32 @@
 // exercise 1
-let a = "chocolate";
-let b = "breakfast";
-console.log ("I eat" + " " + a + " "+ "for" + " " +  b);
+let food = "chocolate";
+let meal = "breakfast";
+console.log ("I eat" + " " + food + " "+ "for" + " " +  meal);
 
 // exercise 2
 let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
 let myWatchedSeriesLength = myWatchedSeries.length ;
 console.log (myWatchedSeriesLength);
 let myWatchedSeriesSentence = "I watched 3 series";
-
 console.log (myWatchedSeriesSentence + " " + myWatchedSeries[0] + " " + myWatchedSeries[1]+ " "+ "and"+ " "+ myWatchedSeries[2] );
+myWatchedSeries.splice (2,2, "friends");
+myWatchedSeries.push ("parks and rec");
+myWatchedSeries.splice (0, 0, "Shera");
+myWatchedSeries.splice (1, 1);
+console.log (myWatchedSeries [1]);
+console.log (myWatchedSeries);
+
+//exercise 4
+  let c;
+    let a = 34;
+    let b = 21;
+
+    console.log(a+b); //first expression
+    // Prediction: 55
+    // Actual:
+
+     a = 2;
+
+    console.log(a+b); //second expression
+    // Prediction: 23 bc of new a
+    // Actual:
