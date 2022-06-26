d7f88f4dae855f400330460cfcae2accb5d026e2
henna storch <henna.storch@gmail.com>
2022-06-26 11:08:06 +0300

    js

---
+++ week2/day1/class/script.js
@@ -0,0 +1,4 @@
+let my_num = 100;
+let a;
+a = 10;
+console.log (my_num + a);
---
+++ week2/day1/exercises/script.js
@@ -0,0 +1,12 @@
+// exercise 1
+let a = "chocolate";
+let b = "breakfast";
+console.log ("I eat" + " " + a + " "+ "for" + " " +  b);
+
+// exercise 2
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let myWatchedSeriesLength = myWatchedSeries.length ;
+console.log (myWatchedSeriesLength);
+let myWatchedSeriesSentence = "I watched 3 series";
+
+console.log (myWatchedSeriesSentence + " " + myWatchedSeries[0] + " " + myWatchedSeries[1]+ " "+ "and"+ " "+ myWatchedSeries[2] );
