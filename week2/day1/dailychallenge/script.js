b77bc3d21bf859e9222c712a2586d9888efd35a4
henna storch <henna.storch@gmail.com>
2022-06-26 12:40:19 +0300

    daily challenge

---
+++ week2/day1/dailychallenge/script.js
@@ -0,0 +1,10 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+fruits.push ("kiwi")
+fruits.sort()
+fruits.splice(0,1)
+console.log (fruits)
+fruits.reverse()
+console.log(fruits)
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log (moreFruits[1][1])
