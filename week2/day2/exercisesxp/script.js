91a8d413cd4199b1c1f7ab0e6d370f8255d53dbe
henna storch <henna.storch@gmail.com>
2022-06-27 14:23:32 +0300

    start exercises

---
+++ week2/day2/exercisesxp/script.js
@@ -0,0 +1,27 @@
+//Create 2 variables, x and y. Each of them should have a different numeric value.
+//Write an if/else statement that checks which number is bigger.
+
+let x = 5;
+let y = 2;
+if ( x > y) {
+	document.write ("x is the bigger number")
+}
+
+
+//Create a variable called newDog where it’s value is “Chihuahua”.
+//Check and display how many letters are in newDog.
+//Display the newDog variable in uppercase and then in lowercase (no need to create new variables, just console.log twice).
+//Check if the variable newDog is equal to “Chihuahua”
+	//if it does, display ‘I love Chihuahuas, it’s my favorite dog breed’
+	//else, console.log ‘I dont care, I prefer cats’
+ let newDog = "chihuahua"
+document.write (newDog.length)
+console.log (newDog. toLowerCase())
+console.log (newDog.toUpperCase())
+if (newDog = "chihuahua") {
+	console.log ("I love Chihuahuas, it’s my favorite dog breed");
+
+}
+else {
+	console.log ("I dont care, I prefer cats")
+}
