2f4b47efa639f1302513b44ab937164413d4090c
henna storch <henna.storch@gmail.com>
2022-06-29 05:02:16 +0300

    star

--- week2/day3/dailychallenge/script.js
+++ week2/day3/dailychallenge/script.js
@@ -1,9 +1,20 @@
 let star = "*"
 
-console.log (star)
 
-while ( star.length < 6) {
-	star = star + "*"
+while ( star.length < 7) {
 	console.log (star)
+	star = star + "*"
+	
 }
 
+
+
+
+
+for (let x = 0; x <= 0; x++) {
+	for (let z = "*"; z.length < 7; z = z + "*") {
+		console.log (z)
+
+	}
+	
+}
