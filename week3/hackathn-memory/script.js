2be04b038fd9b28e723accf029f0f6e36ba6d908
henna storch <henna.storch@gmail.com>
2022-07-07 14:50:28 +0300

    check for matches

--- week3/hackathn-memory/script.js
+++ week3/hackathn-memory/script.js
@@ -2,7 +2,15 @@
 // place them on grid as colored square
 
 let game = document.getElementById("Memorygame")
-console.log(game)
+
+let pics = ["banana.jpg", "kiwi.jpg", 
+			"pear.jpg", "strawberry.jpg", 
+			"apple.png", "cherry.png",
+			"lemon.png", "watermelon.png", "banana.jpg", "kiwi.jpg", 
+			"pear.jpg", "strawberry.jpg", 
+			"apple.png", "cherry.png",
+			"lemon.png", "watermelon.png"]
+let Check = []
 
 for (var i = 0; i < 16; i++) {
 	 let div = document.createElement('div');
@@ -12,14 +20,38 @@ for (var i = 0; i < 16; i++) {
 	 game.appendChild(div)
 	 div.style.backgroundColor = "blue"
 	 div.style.height = 135 + 'px'
-
-	 console.log(div)
+	 let img = document.createElement('img')
+	 img.setAttribute('src', pics[i])
+	 img.classList.add ("hide")
+	 img.id = pics[i]
+	 img.style.height = 100 + 'px'
+	 img.style.margin = 'auto'
+	 div.appendChild (img)
+	 div.addEventListener('click',function(){
+	 	div.firstElementChild.style.display = 'block'
+	 	console.log(img)
+	 	Check.push (img)
+	 	
+	 	if (Check.length == 2){
+	 		console.log (Check)
+	 		if (Check[0] = Check[1]) {
+	 			console.log("match")
+	 		}
+	 		else {
+	 			console.log('not a match')
+	 		}
+	 	}
+	 })
 	
 }
 
+
+
+//create array of pictures
+
 // when clicked - show image
+
+
 // check image - when 2nd image is clicked if they are the same nothing happens
 // if the images are different, go back to image
 // if all divs show an image display "congrats"
-
-
