a5c8a7d7171953f8ed4cf6c048a489bdb39e76be
henna storch <henna.storch@gmail.com>
2022-07-11 12:14:36 +0300

    daily challenge

--- week4/day2/dailychallenge.py
+++ week4/day2/dailychallenge.py
@@ -5,7 +5,24 @@ length = int(length)
 current_number= 1
 multiplied = []
 while current_number <= length:
-    number = number * current_number
+    x = 1
+    x = number * current_number
     current_number += 1
-    multiplied.append(number)
+    multiplied.append(x)
 print(multiplied)
+
+
+
+user_string = input('please input a word')
+user_string = user_string + " "
+x = 1
+no_duplicates = set()
+word = ""
+
+while x < len(user_string):
+    if user_string[x-1] != user_string[x]:
+        word = word + user_string [x-1]
+    x += 1
+
+print(word)
+
