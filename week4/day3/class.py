c96abcec7568a0c6375d7a2ea0c6dd0af8f767b3
henna storch <henna.storch@gmail.com>
2022-07-13 04:07:34 +0300

    day 3

---
+++ week4/day3/class.py
@@ -0,0 +1,12 @@
+sample_dict = { 
+   "class":{ 
+      "student":{ 
+         "name":"Mike",
+         "marks":{ 
+            "physics":70,
+            "history":80
+         }
+      }
+   }
+}
+print(sample_dict['class']['student']['marks']['history'])
