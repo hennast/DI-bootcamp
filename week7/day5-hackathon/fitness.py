50d442a9090cb5e00d7457861a573f832fe2f1f3
henna <henna.storch@gmail.com>
2022-08-04 12:20:40 +0300

    hackathon

---
+++ week7/day5-hackathon/fitness.py
@@ -0,0 +1,50 @@
+
+import random
+
+
+
+class Exercise:
+    exercises = {}
+    def __init__(self, exercise: str, level: float):
+        self.exercise = exercise
+        self.level = level
+        if self.level not in Exercise.exercises:
+            Exercise.exercises[self.level] = [self]
+        else:
+            Exercise.exercises[self.level].append(self)
+        
+   
+    def __repr__(self) -> str:
+        return str(self.__dict__)
+
+beginner_lift = Exercise("lift 5", 1)
+beginner_run = Exercise("run 3", 1)
+intermediate_lift = Exercise("lift 10", 3.5)
+intermediate_run = Exercise("run 12", 3.8)
+
+
+
+class Workout:
+       
+    def __init__(self, goal):
+        self.goal = goal
+    def create_workout(self):
+        available_exercises = Exercise.exercises
+        chosen_exercices = []
+        while self.goal >= sorted(available_exercises)[0]:
+
+            level = random.choice(list(available_exercises.keys()))
+
+            if level > self.goal:
+                continue
+    
+            chosen_exercices.append(random.choice(available_exercises[level]).exercise)
+    
+            self.goal -= level
+        print("workout:", chosen_exercices)
+
+my_goal = int(input("what level workout would you like"))
+
+my_workout = Workout(my_goal)
+my_workout.create_workout()
+
