94828243e3fc4d6a6ec25f3b0a7aa24d04c71bc3
henna storch <henna.storch@gmail.com>
2022-10-02 12:29:04 +0300

    new stuff

--- final project/personal_trainer/workout/models.py
+++ final project/personal_trainer/workout/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class workout(models.Model):
+    Exercise_name = models.CharField(max_length=50)
+    Exercise_type = models.CharField(max_length=40)
+    Exercise_difficulty = models.FloatField()
+    
--- final project/personal_trainer/workout/urls.py
+++ final project/personal_trainer/workout/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import homepage, cardio
+from .views import homepage, cardio, strength, speed, endurance
 
 
-
+app_name = 'workout'
 urlpatterns = [
     path('', homepage, name='home'),
-    path('cardio', cardio, name='cardio'),
+    path('cardio', cardio, name="cardio"),
+    path('strength', strength, name="strength"),
+    path('speed', speed, name="speed"),
+    path('endurance', endurance, name="endurance"),
 ]
