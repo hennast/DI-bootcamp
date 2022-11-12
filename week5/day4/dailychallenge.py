db8ee9869ed4f097e25cbb1547b12ce50e7966f4
henna storch <henna.storch@gmail.com>
2022-11-12 19:15:58 +0200

    dictionary fix

--- week5/day4/dailychallenge.py
+++ week5/day4/dailychallenge.py
@@ -17,41 +17,48 @@ class Text:
             print(None)
         else:
             print(f"your word was found {timesInString} times")
-    def dictionary(self, dictionary):
+    def dictionary(self):
         #creates a dictionary of how many times each word
         stringAslist = self.string.split(" ")
         for word in stringAslist:
-            if word in dictionary.keys():
-                dictionary[word] += 1
+            if word in StringDict.keys():
+                StringDict[word] += 1
             else:
-                dictionary[word] = 1
+                StringDict[word] = 1
         return self.dictionary
-    def mostCommon(self, dictionary: dict):
+    def mostCommon(self):
         #a method that returns the most common word in the text.
+        global StringDict
+        self.dictionary()
         x = 0
         mostCommonWord = ""
-        for key, value in dictionary.items():
+        for key, value in StringDict.items():
             if value > x:
                 x = value
                 mostCommonWord = key
             else:
                 pass
         print(f"the most common word is {mostCommonWord} which appears {x} times")
-    def unique(self, dictionary: dict):
+        StringDict = {}
+        return StringDict
+    def unique(self):
         #a method that returns a list of all the unique words in the text.
+        global StringDict
+        self.dictionary()
         uniqueWords = []
-        for key, value in dictionary.items():
+        for key, value in StringDict.items():
             if value == 1:
                 uniqueWords.append(key)
             else:
                 pass
         print(uniqueWords)
+        StringDict = {}
+        return StringDict
 
 test = Text(sampleSentence)
 test.count("good")
-test.dictionary(StringDict)
-test.mostCommon(StringDict)
-test.unique(StringDict)
+test.mostCommon()
+test.unique()
 
 
 
